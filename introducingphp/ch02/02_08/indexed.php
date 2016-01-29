64d8dbe3e1351601d882c934a47aa3eebaaf4886
Tim Klein <tklein@outlook.com>
2016-01-28 23:49:06 -0700

    first commit

---
+++ introducingphp/ch02/02_08/indexed.php
@@ -0,0 +1,11 @@
+<?php
+// $characters = array('Arthur Dent', 'Ford Prefect', 'Zaphod Beeblebrox');
+$characters = ['Arthur Dent', 'Ford Prefect', 'Zaphod Beeblebrox'];
+
+$characters[] = 'Marvin';
+$characters[] = 'Slartibartfast';
+
+// print_r($characters);
+
+echo "$characters[0]";
+
