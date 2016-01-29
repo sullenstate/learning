b8d1ebc4162a2224b956b59ec65cdf4dccc23a69
Tim Klein <tklein@outlook.com>
2016-01-29 12:32:13 -0700

    Sync Progress

--- introducingphp/ch03/03_07/different.php
+++ introducingphp/ch03/03_07/different.php
@@ -6,18 +6,27 @@
     <link href="styles.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+
+<?php
+$member = 0;
+?>
+
 <div id="wrapper">
+
     <header class="both">
         <h1>Nonesuch Club</h1>
         <p>Welcome one and all to a really exclusive club for PHP developers. So exclusive, it doesn't exist!</p>
     </header>
 
+<?php
+
+if(!$member) {?>
     <section class="non-members">
         <h2>Become a Member</h2>
         <p>Useful information for non-members. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae doloribus esse itaque necessitatibus nobis placeat repellat temporibus voluptas. Architecto maxime quisquam soluta veniam? Aliquid consequuntur cum eligendi enim, natus non!</p>
         <p>Accusamus animi architecto asperiores aspernatur at dolor eaque error est illum ipsam libero nam nemo nisi, officiis pariatur quae quidem quod sed sunt totam velit veniam vero voluptas voluptatem voluptatibus?</p>
     </section>
-
+<?php } else { ?>
     <section class="members">
         <h2>Members' News</h2>
         <p>For members's eyes only. Amet at, cum delectus deserunt dolorem doloribus ea esse eum eveniet facilis fuga illo in itaque iusto laudantium minus molestiae necessitatibus nobis, non obcaecati odit ratione repellendus rerum. Odit, placeat.</p>
@@ -29,7 +38,7 @@
         </ul>
         <p>Aliquid cum ex expedita illum ipsa maxime, minus, natus nihil officia praesentium quas, quisquam ratione sapiente. Aliquid animi beatae fugit in magnam necessitatibus, nihil nobis quod repudiandae saepe. Similique, ullam?</p>
     </section>
-
+<?php }; ?>
     <section>
         <div class="both">
         <h3>About this Club</h3>
@@ -37,12 +46,14 @@
             <p>Accusamus architecto, aspernatur commodi consectetur doloremque esse est illum impedit inventore itaque iusto laboriosam nesciunt nostrum nulla optio provident quae quidem sapiente sequi tempore vel veritatis voluptatem. Magni, quos rerum?</p>
         </div>
 
+<?php
+if ($member) { ?>
         <div class="members">
             <h3>Information for Members Only</h3>
             <p>Don't let non-members into the secret! Dolor dolores dolorum harum, illum molestias quia quidem ullam. A accusamus alias assumenda iure provident quae quos! A, alias cumque distinctio excepturi, incidunt mollitia nemo neque quam suscipit ut voluptate.</p>
         </div>
     </section>
-
+<?php } ?>
     <footer class="both">
         <p>&copy; 2015 Nonesuch Club</p>
     </footer>
--- introducingphp/ch04/04_04/integer_division.php
+++ introducingphp/ch04/04_04/integer_division.php
@@ -1,7 +1,9 @@
 <?php
 // Convert $total_minutes to hours and minutes.
 
-$total_minutes = 318;
+$total_minutes = 340;
 $minutes = $total_minutes % 60;
+$hours = floor($total_minutes/60);
+// $hours = round($total_minutes/60);
 
-echo "Time taken was ?? hours $minutes minutes";
+echo "Time taken was " . $hours . " hours $minutes minutes";
