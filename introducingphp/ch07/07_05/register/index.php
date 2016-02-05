eaf095b8bfad3e88447360dcf11c37215269394c
Tim Klein <tklein@outlook.com>
2016-02-04 23:49:30 -0700

    Progress Updates

--- introducingphp/ch07/07_05/register/index.php
+++ introducingphp/ch07/07_05/register/index.php
@@ -1,3 +1,6 @@
+<?php set_include_path( get_include_path() . PATH_SEPARATOR . '/Applications/MAMP/htdocs/introducingphp/ch07/07_05/includes'); 
+$siteroot = '/introducingphp/ch07/07_05';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,23 +13,7 @@
 <body id="page_register">
 <div class="wrapper">
   <header>
-    <div class="branding"> <img src="../images/ralogo_monogram.png" alt="Logo">
-      <p>Roux Academy<br>
-        Art • Media • Design</p>
-    </div>
-    <div class="description">
-      <h1>Roux Academy 2016 Art Conference</h1>
-      <p>Join over 500 hundred of the most creative and brilliant minds of art colleges all around the world for five days of lectures by world-renowned art scholars and artists, and seven days and nights of gallery exhibits featuring the best in contemporary art, including painting, sculpture, and more, in the beautiful halls of Hotel Contempo in the heart of Seattle.</p>
-    </div>
-    <nav>
-      <ol>
-        <li><a href="../index.php">HOME</a></li>
-        <li><a href="../artists.php">ARTISTS</a></li>
-        <li><a href="../schedule/index.php">SCHEDULE</a></li>
-        <li><a href="../venue.php">VENUE/TRAVEL</a></li>
-        <li><a href="index.php">REGISTER</a></li>
-      </ol>
-    </nav>
+   <?php include 'header.php'; ?>
   </header>
   <div id="rotator"> <img src="../images/art01.jpg" alt="Artwork from show"> </div>
   <!-- Rotator -->
